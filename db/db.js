7de4e32d30ae00013dd8466a5bb87eb081c943ba
norvalbv <benjinorval@gmail.com>
2022-03-23 17:32:03 +0000

    test

--- db/db.js
+++ db/db.js
@@ -18,14 +18,14 @@ const pool = new Pool(
   process.env.NODE_ENV === "production" ? proConfig : devConfig
 );
 
-pool.query(
-  "SELECT table_schema, table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-  }
-);
+// pool.query(
+//   "SELECT table_schema, table_name FROM information_schema.tables;",
+//   (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//       console.log(JSON.stringify(row));
+//     }
+//   }
+// );
 
 module.exports = pool;
